f59b94deb2e3e2e2383684511f8c47b664e2e12b
Kevin Netherton <kevin.netherton@gov.bc.ca>
2020-02-26 18:00:56 -0800

    saving the dict questions

---
+++ 05_Dictionaries_and_complex_data/DictQuestions.py
@@ -0,0 +1,16 @@
+'''
+Dictionary Questions
+
+'''
+
+# Step 1.
+# import the json data contained in the file ../data/players_t8.json
+
+# Step 2. 
+# verify that your data import was successful by printing the first player in the struct
+
+# Step 3. 
+# create an iteration that iterates over each player
+
+# Step 4. 
+# create a new list, and populate it with players that are from canada.
